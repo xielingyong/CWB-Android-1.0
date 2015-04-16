46f0b52160d9e5c340679d8eedc3cb46364cfaed
xielingyong <2867306902@qq.com>
2015-04-16 15:50:15 +0800

    谢玲勇

---
+++ CWB/src/com/xinmengxiang/cwb/adapter/TestFragmentAdapter.java
@@ -0,0 +1,60 @@
+package com.xinmengxiang.cwb.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.View;
+
+import com.viewpagerindicator.IconPagerAdapter;
+import com.xinmengxiang.cwb.activity.R;
+import com.xinmengxiang.cwb.fragment.TestFragment;
+
+public class TestFragmentAdapter extends FragmentPagerAdapter implements IconPagerAdapter {
+    protected static final String[] CONTENT = new String[] { "首页", "商品", "健康", "品种","医疗" };
+    protected static final int[] ICONS = new int[] {
+            R.drawable.perm_group_calendar,
+            R.drawable.perm_group_camera,
+            R.drawable.perm_group_device_alarms,
+            R.drawable.perm_group_location
+    };
+
+    private int mCount = CONTENT.length;
+
+    public TestFragmentAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return TestFragment.newInstance(CONTENT[position]);
+    }
+
+    @Override
+    public int getCount() {
+        return mCount;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+      return TestFragmentAdapter.CONTENT[position % CONTENT.length];
+    }
+
+    @Override
+    public int getIconResId(int index) {
+      return ICONS[index % ICONS.length];
+    }
+
+    public void setCount(int count) {
+        if (count > 0 && count <= 10) {
+            mCount = count;
+            notifyDataSetChanged();
+        }
+    }
+    
+
+    
+    
+    
+}
---
+++ CWB/src/com/xinmengxiang/cwb/fragment/TestFragment.java
@@ -0,0 +1,27 @@
+package com.xinmengxiang.cwb.fragment;
+
+import android.support.v4.app.Fragment;
+
+
+public final class TestFragment extends Fragment {
+
+    public static Fragment newInstance(String content) {
+
+    	Fragment fragment=null;
+    	
+    	if(content.equals("首页")){
+    		fragment=new Fragment1();
+    	}else if(content.equals("商品")){
+    		fragment=new Fragment2();
+    	}else if(content.equals("健康")){
+    		fragment=new Fragment3();
+    	}else if(content.equals("品种")){
+    		fragment=new Fragment4();
+    	}else if(content.equals("医疗")){
+    		fragment=new Fragment5();
+    	}
+    	
+        return fragment;
+    }
+
+}

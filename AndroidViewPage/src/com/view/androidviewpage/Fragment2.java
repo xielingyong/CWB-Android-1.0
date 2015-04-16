4ab1c8438630ce2c2591d129f5646e484f50d5c5
xielingyong <2867306902@qq.com>
2015-04-16 14:57:39 +0800

    谢玲勇
    宠物帮1.0测试版

---
+++ AndroidViewPage/src/com/view/androidviewpage/Fragment2.java
@@ -0,0 +1,19 @@
+package com.view.androidviewpage;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public final class Fragment2 extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return inflater.inflate(R.layout.fragment2, container,false);
+	}
+
+}
---
+++ AndroidViewPage/src/com/view/androidviewpage/MainActivity.java
@@ -0,0 +1,35 @@
+package com.view.androidviewpage;
+
+import com.viewpagerindicator.TitlePageIndicator;
+import com.viewpagerindicator.TitlePageIndicator.IndicatorStyle;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.view.Window;
+
+
+public class MainActivity extends FragmentActivity {
+
+	private ViewPager mPager=null;
+	private TitlePageIndicator mIndicator=null;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+
+		TestFragmentAdapter  mAdapter = new TestFragmentAdapter(getSupportFragmentManager());
+
+        mPager = (ViewPager)findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+
+
+        TitlePageIndicator indicator = (TitlePageIndicator)findViewById(R.id.indicator);
+        indicator.setViewPager(mPager);
+        indicator.setFooterIndicatorStyle(IndicatorStyle.Triangle);
+        mIndicator = indicator;
+	}
+
+
+}
